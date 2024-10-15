c491957f9372145e3b928221c2d7a093ac1c452b
nyanlinhtet11 <89507759+sweetmovie@users.noreply.github.com>
2024-10-16 01:15:45 +0630

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 24344383
+API_HASH     = "5177fe64d5145b2f2fd27508a92346db"
+BOT_TOKEN    = "8122747834:AAHjLNYqMNi0Vhr5q4vQf4AgdB67jutNJV8"
+SESSION      = "BQFzdz8AjqCM1rMZ4AP9X9CMzYHbXmCwqjcAU1yyVTeVUcdcyK9u8CLTv5h-zoqhaPedCIWBrov6336zlxoi01GiUoIo-tcFBx5Qql7S9454Pp62YmgGbW-m7q6hiQjg651LZO17GyyhO4EfZi9k5RL7ssbhHGQXXiJ2qGBImSmWSPAjcnugMldzJnxVjL5KrXoU-ZfGB9ce9KkvaN1U-yqOCMsibu2qAlwH5AFP2yp9aQKtnWV1OZTnkZhU9Zmm8H4u_nf2I4a-CeUy9AC8jiDbZq7Pp-vi2aC3rImMgtOx1kmjgZaqWToHP_2f32001aIz4JA_7bwCnjXpva5UdRZ11qGDWQAAAAG5UrEfAA"
+DATABASE_URI = "mongodb+srv://nyanlynnhtet477:nyanlynnhtet477@cluster0.48mkp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002397135618
+ADMIN        = 7404171551
+CHANNEL      = "@mmsubmovieMflixer"
